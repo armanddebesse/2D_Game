d6309323f61c46b3d02ab5eee900f05232ad0e8b
Helrho <armand-gab@hotmail.fr>
2022-05-08 23:26:28 +0100

    [ADD] Player is mooving with animation

---
+++ src/entity/Player.java
@@ -0,0 +1,123 @@
+package entity;
+
+import main.KeyHandler;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import main.GamePanel;
+
+public class Player extends Entity{
+	GamePanel gamePanel;
+	KeyHandler keyHandler;
+	
+	public Player(GamePanel gp, KeyHandler keyH) {
+		this.gamePanel = gp;
+		this.keyHandler = keyH;
+		
+		setDefaultValues();
+		getPlayerImage();
+	}
+	
+	public void setDefaultValues() {
+		x = 100;
+		y = 100;
+		speed = 2;
+		direction = Direction.DOWN;
+	}
+	
+	public void getPlayerImage() {
+		try {
+			up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+			up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+			down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+			down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+			right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+			right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+			left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+			left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void update() {
+		if(keyHandler.upPressed || keyHandler.downPressed || keyHandler.leftPressed || keyHandler.rightPressed) {
+
+			if(keyHandler.upPressed) {
+				direction = Direction.UP;
+				y -= speed;
+			}
+			else if(keyHandler.downPressed) {
+				direction = Direction.DOWN;
+				y += speed;
+			}
+			else if(keyHandler.leftPressed) {
+				direction = Direction.LEFT;
+				x -= speed;
+			}
+			else if(keyHandler.rightPressed) {
+				direction = Direction.RIGHT;
+				x += speed;
+			}
+			
+			spriteCounter++;
+			if(spriteCounter > 20) {
+				if(spriteNumber == 1) {
+					spriteNumber = 2;
+				}
+				else if(spriteNumber == 2) {
+					spriteNumber = 1;
+				}
+				spriteCounter = 0;
+			}
+		}
+		
+	}
+	
+	public void draw(Graphics2D graph2D) {
+		BufferedImage image = null;
+		switch(direction) {
+		case DOWN:
+			if(spriteNumber == 1) {
+				image = down1;
+			}
+			else if(spriteNumber == 2) {
+				image = down2;
+			}
+			break;
+		case LEFT:
+			if(spriteNumber == 1) {
+				image = left1;
+			}
+			else if(spriteNumber == 2) {
+				image = left2;
+			}
+			break;
+		case RIGHT:
+			if(spriteNumber == 1) {
+				image = right1;
+			}
+			else if(spriteNumber == 2) {
+				image = right2;
+			}
+			break;
+		case UP:
+			if(spriteNumber == 1) {
+				image = left1;
+				image = up1;
+			}
+			else if(spriteNumber == 2) {
+				image = up2;
+			}
+			break;
+		default:
+			break;
+		}
+		graph2D.drawImage(image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
+	}
+}
