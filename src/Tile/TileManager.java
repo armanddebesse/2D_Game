8f1b980a09f9f4772b1356f91f1429a166a30ebd
MSI\Killian <killian.legendre@viacesi.fr>
2022-05-09 21:30:17 +0200

    change player, and continue map

--- src/Tile/TileManager.java
+++ src/Tile/TileManager.java
@@ -34,8 +34,23 @@ public class TileManager {
 		}
 	}
 	public void draw(Graphics2D g2) {
-		g2.drawImage(tile[0].image, 0, 0, gamePanel.tileSize,gamePanel.tileSize,null);
-		g2.drawImage(tile[1].image, 0, 48, gamePanel.tileSize,gamePanel.tileSize,null);
-		g2.drawImage(tile[2].image, 0, 96, gamePanel.tileSize,gamePanel.tileSize,null);
+		
+		int col = 0;
+		int row = 0;
+		int x = 0;
+		int y = 0;
+		
+		while(col < gamePanel.maxScreenCol && row < gamePanel.maxScreenRow) {
+			g2.drawImage(tile[0].image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
+			
+			col++;
+			x += gamePanel.tileSize;
+			 if(col == gamePanel.maxScreenCol) {
+				 col = 0;
+				 x = 0;
+				 row++;
+				 y += gamePanel.tileSize;
+			 }
+		}
 	}
 }
