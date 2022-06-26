c87c933179c13652a9f8ddad0b9604cdc6cff09a
Helrho <armand-gab@hotmail.fr>
2022-06-26 16:44:08 +0100

    [ADD] Map loader can load multiple layers

--- src/main/CollisionHandler.java
+++ src/main/CollisionHandler.java
@@ -10,51 +10,53 @@ public class CollisionHandler {
 	}
 	
 	public void checkTile(Entity entity) {
-		int entityLeftWorldX = entity.worldX + entity.solidArea.x;
-		int entityRightWorldX = entity.worldX + entity.solidArea.x + entity.solidArea.width;
-		int entityTopWorldY = entity.worldY + entity.solidArea.y;
-		int entityBottomWorldY = entity.worldY + entity.solidArea.y + entity.solidArea.height;
-		
-		int entityLeftCol = entityLeftWorldX / gamePanel.tileSize;
-		int entityRightCol = entityRightWorldX / gamePanel.tileSize;
-		int entityTopRow = entityTopWorldY / gamePanel.tileSize;
-		int entityBottomRow = entityBottomWorldY / gamePanel.tileSize;
-		
-		int tileNum1, tileNum2;
-		
-		switch (entity.direction) {
-			case UP:
-				entityTopRow = (entityTopWorldY - entity.speed) / gamePanel.tileSize;
-				tileNum1 = gamePanel.currentMap.data[entityLeftCol][entityTopRow];
-				tileNum2 = gamePanel.currentMap.data[entityRightCol][entityTopRow];
-				if (gamePanel.currentMap.tiles[tileNum1].collision || gamePanel.currentMap.tiles[tileNum2].collision) {
-					entity.collisionOn = true;
-				}
-				break;
-			case DOWN:
-				entityBottomRow = (entityBottomWorldY + entity.speed) / gamePanel.tileSize;
-				tileNum1 = gamePanel.currentMap.data[entityLeftCol][entityBottomRow];
-				tileNum2 = gamePanel.currentMap.data[entityRightCol][entityBottomRow];
-				if (gamePanel.currentMap.tiles[tileNum1].collision || gamePanel.currentMap.tiles[tileNum2].collision) {
-					entity.collisionOn = true;
-				}
-				break;
-			case LEFT:
-				entityLeftCol = (entityLeftWorldX - entity.speed) / gamePanel.tileSize;
-				tileNum1 = gamePanel.currentMap.data[entityLeftCol][entityTopRow];
-				tileNum2 = gamePanel.currentMap.data[entityLeftCol][entityBottomRow];
-				if (gamePanel.currentMap.tiles[tileNum1].collision || gamePanel.currentMap.tiles[tileNum2].collision) {
-					entity.collisionOn = true;
-				}
-				break;
-			case RIGHT:
-				entityRightCol = (entityRightWorldX + entity.speed) / gamePanel.tileSize;
-				tileNum1 = gamePanel.currentMap.data[entityRightCol][entityTopRow];
-				tileNum2 = gamePanel.currentMap.data[entityRightCol][entityBottomRow];
-				if (gamePanel.currentMap.tiles[tileNum1].collision || gamePanel.currentMap.tiles[tileNum2].collision) {
-					entity.collisionOn = true;
-				}
-				break;
+		for (int i = 0; i < gamePanel.currentMap.layers.size(); i++) {
+			int entityLeftWorldX = entity.worldX + entity.solidArea.x;
+			int entityRightWorldX = entity.worldX + entity.solidArea.x + entity.solidArea.width;
+			int entityTopWorldY = entity.worldY + entity.solidArea.y;
+			int entityBottomWorldY = entity.worldY + entity.solidArea.y + entity.solidArea.height;
+			
+			int entityLeftCol = entityLeftWorldX / gamePanel.tileSize;
+			int entityRightCol = entityRightWorldX / gamePanel.tileSize;
+			int entityTopRow = entityTopWorldY / gamePanel.tileSize;
+			int entityBottomRow = entityBottomWorldY / gamePanel.tileSize;
+			
+			int tileNum1, tileNum2;
+			
+			switch (entity.direction) {
+				case UP:
+					entityTopRow = (entityTopWorldY - entity.speed) / gamePanel.tileSize;
+					tileNum1 = gamePanel.currentMap.layers.get(i)[entityLeftCol][entityTopRow];
+					tileNum2 = gamePanel.currentMap.layers.get(i)[entityRightCol][entityTopRow];
+					if ((tileNum1 != 0 && gamePanel.currentMap.tiles[tileNum1].collision) || (tileNum2 != 0 && gamePanel.currentMap.tiles[tileNum2].collision)) {
+						entity.collisionOn = true;
+					}
+					break;
+				case DOWN:
+					entityBottomRow = (entityBottomWorldY + entity.speed) / gamePanel.tileSize;
+					tileNum1 = gamePanel.currentMap.layers.get(i)[entityLeftCol][entityBottomRow];
+					tileNum2 = gamePanel.currentMap.layers.get(i)[entityRightCol][entityBottomRow];
+					if ((tileNum1 != 0 && gamePanel.currentMap.tiles[tileNum1].collision) || (tileNum2 != 0 && gamePanel.currentMap.tiles[tileNum2].collision)) {
+						entity.collisionOn = true;
+					}
+					break;
+				case LEFT:
+					entityLeftCol = (entityLeftWorldX - entity.speed) / gamePanel.tileSize;
+					tileNum1 = gamePanel.currentMap.layers.get(i)[entityLeftCol][entityTopRow];
+					tileNum2 = gamePanel.currentMap.layers.get(i)[entityLeftCol][entityBottomRow];
+					if ((tileNum1 != 0 && gamePanel.currentMap.tiles[tileNum1].collision) || (tileNum2 != 0 && gamePanel.currentMap.tiles[tileNum2].collision)) {
+						entity.collisionOn = true;
+					}
+					break;
+				case RIGHT:
+					entityRightCol = (entityRightWorldX + entity.speed) / gamePanel.tileSize;
+					tileNum1 = gamePanel.currentMap.layers.get(i)[entityRightCol][entityTopRow];
+					tileNum2 = gamePanel.currentMap.layers.get(i)[entityRightCol][entityBottomRow];
+					if ((tileNum1 != 0 && gamePanel.currentMap.tiles[tileNum1].collision) || (tileNum2 != 0 && gamePanel.currentMap.tiles[tileNum2].collision)) {
+						entity.collisionOn = true;
+					}
+					break;
+			}
 		}
 	}
 	public int checkObject(Entity entity, boolean isPlayer) {
--- src/tile/Map.java
+++ src/tile/Map.java
@@ -3,9 +3,9 @@ package tile;
 import java.awt.Graphics2D;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -16,28 +16,33 @@ public class Map {
 	GamePanel gamePanel;
 	public int width;
 	public int height;
-	public int data[][];
+	public ArrayList<int[][]> layers;
 	public Tile[] tiles;
 	
 	public Map(String path, GamePanel _gamePanel) {
 		gamePanel = _gamePanel;
+		layers = new ArrayList<int[][]>();
 		try {
 			//LOAD JSON
 			JSONObject map = new JSONObject(Files.readString(Path.of(path)));
-			JSONArray mapJsonArray = map.getJSONArray("layers").getJSONObject(0).getJSONArray("data");
-			
 			width = map.getInt("width");
 			height = map.getInt("height");
 			
+			JSONArray layersArray = map.getJSONArray("layers");
+
 			//LOAD MAP
-			data = new int[width][height];
-			
-			int k = 0;
-			for (int i = 0; i < width; i++) {
-				for (int j = 0; j < height; j++) {
-					data[j][i] = mapJsonArray.getInt(k);
-					k++;
+			for (int i = 0; i < layersArray.length(); i++) {
+				JSONArray mapJsonArray = layersArray.getJSONObject(i).getJSONArray("data");
+				int[][] currentLayer = new int[width][height];
+				
+				int k = 0;
+				for (int l = 0; l < width; l++) {
+					for (int j = 0; j < height; j++) {
+						currentLayer[j][l] = mapJsonArray.getInt(k);
+						k++;
+					}
 				}
+				layers.add(currentLayer);
 			}
 			
 			//LOAD TILES
@@ -78,52 +83,55 @@ public class Map {
 		}
 	}
 	public void draw(Graphics2D graph2D) {
-		int worldCol = 0;
-		int worldRow = 0;
-		
-		while(worldCol < gamePanel.maxWorldCol && worldRow < gamePanel.maxWorldRow) {
+		for (int i = 0; i < layers.size(); i++) {
+			int worldCol = 0;
+			int worldRow = 0;
 			
-			int tileNumber = data[worldCol][worldRow];
-			
-			int worldX = worldCol * gamePanel.tileSize;
-			int worldY = worldRow * gamePanel.tileSize;
-			int screenX = worldX - gamePanel.player.worldX + gamePanel.player.screenX;
-			int screenY = worldY - gamePanel.player.worldY + gamePanel.player.screenY;
-			
-			// Stop moving camera at the edge of map
-			if (gamePanel.player.screenX > gamePanel.player.worldX) {
-				screenX = worldX;
-			}
-			if (gamePanel.player.screenY > gamePanel.player.worldY) {
-				screenY = worldY;
-			}
-			int rightOffset = gamePanel.screenWidth - gamePanel.player.screenX;
-			if (rightOffset > gamePanel.worldWidth - gamePanel.player.worldX) {
-				screenX = gamePanel.screenWidth - (gamePanel.worldWidth - worldX);
-			}
-			int bottomOffset = gamePanel.screenHeight - gamePanel.player.screenY;
-			if (bottomOffset > gamePanel.worldHeight - gamePanel.player.worldY) {
-				screenY = gamePanel.screenHeight - (gamePanel.worldWidth - worldY);
-			}
-			
-			
-			if ((worldX + gamePanel.tileSize> gamePanel.player.worldX - gamePanel.player.screenX &&
-				 worldX - gamePanel.tileSize < gamePanel.player.worldX + gamePanel.player.screenX &&
-				 worldY + gamePanel.tileSize > gamePanel.player.worldY - gamePanel.player.screenY &&
-				 worldY - gamePanel.tileSize < gamePanel.player.worldY + gamePanel.player.screenY) ||
-				 gamePanel.player.screenX > gamePanel.player.worldX ||
-				 gamePanel.player.screenY > gamePanel.player.worldY ||
-				 rightOffset > gamePanel.worldWidth - gamePanel.player.worldX ||
-				 bottomOffset > gamePanel.worldHeight - gamePanel.player.worldY) {
+			while(worldCol < gamePanel.maxWorldCol && worldRow < gamePanel.maxWorldRow) {
 				
-				graph2D.drawImage(tiles[tileNumber].image, screenX, screenY, null);
-			}
-			
-			worldCol++;
-			
-			if(worldCol == gamePanel.maxWorldCol) {
-				worldCol = 0;
-				worldRow++;
+				int tileNumber = layers.get(i)[worldCol][worldRow];
+				
+				int worldX = worldCol * gamePanel.tileSize;
+				int worldY = worldRow * gamePanel.tileSize;
+				int screenX = worldX - gamePanel.player.worldX + gamePanel.player.screenX;
+				int screenY = worldY - gamePanel.player.worldY + gamePanel.player.screenY;
+				
+				// Stop moving camera at the edge of map
+				if (gamePanel.player.screenX > gamePanel.player.worldX) {
+					screenX = worldX;
+				}
+				if (gamePanel.player.screenY > gamePanel.player.worldY) {
+					screenY = worldY;
+				}
+				int rightOffset = gamePanel.screenWidth - gamePanel.player.screenX;
+				if (rightOffset > gamePanel.worldWidth - gamePanel.player.worldX) {
+					screenX = gamePanel.screenWidth - (gamePanel.worldWidth - worldX);
+				}
+				int bottomOffset = gamePanel.screenHeight - gamePanel.player.screenY;
+				if (bottomOffset > gamePanel.worldHeight - gamePanel.player.worldY) {
+					screenY = gamePanel.screenHeight - (gamePanel.worldWidth - worldY);
+				}
+				
+				
+				if ((worldX + gamePanel.tileSize> gamePanel.player.worldX - gamePanel.player.screenX &&
+					 worldX - gamePanel.tileSize < gamePanel.player.worldX + gamePanel.player.screenX &&
+					 worldY + gamePanel.tileSize > gamePanel.player.worldY - gamePanel.player.screenY &&
+					 worldY - gamePanel.tileSize < gamePanel.player.worldY + gamePanel.player.screenY) ||
+					 gamePanel.player.screenX > gamePanel.player.worldX ||
+					 gamePanel.player.screenY > gamePanel.player.worldY ||
+					 rightOffset > gamePanel.worldWidth - gamePanel.player.worldX ||
+					 bottomOffset > gamePanel.worldHeight - gamePanel.player.worldY) {
+					if (tileNumber != 0) {
+						graph2D.drawImage(tiles[tileNumber].image, screenX, screenY, null);
+					}
+				}
+				
+				worldCol++;
+				
+				if(worldCol == gamePanel.maxWorldCol) {
+					worldCol = 0;
+					worldRow++;
+				}
 			}
 		}
 	}
